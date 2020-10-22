5284c7c5e232d478851c93fb8bce76148ba2ca4c
KAWilson1 <kylewilson10@gmail.com>
2020-10-22 10:44:43 -0400

    Initial POC
    Created limited POC using specific location and date range. secret.py was not uploaded to hide API key

---
+++ proof_of_concept.py
@@ -0,0 +1,33 @@
+import secret
+
+from weatherbit.api import Api
+
+api = Api(secret.API_KEY)
+api.set_granularity('daily')
+
+history = api.get_history(city="Carmel", state="Indiana", country="US", start_date="2020-10-18", end_date="2020-10-19")
+for day in history.get_series(['pres']):
+	date = day['datetime'].strftime("%m/%d/%y")
+	print("Pressure on " + date, day['pres'] / 33.864, "inHg") #convert milibar to inHg
+
+history = api.get_history(city="Carmel", state="Indiana", country="US", start_date="2020-10-19", end_date="2020-10-20")
+for day in history.get_series(['pres']):
+	date = day['datetime'].strftime("%m/%d/%y")
+	print("Pressure on " + date, day['pres'] / 33.864, "inHg") #convert milibar to inHg
+
+history = api.get_history(city="Carmel", state="Indiana", country="US", start_date="2020-10-20", end_date="2020-10-21")
+for day in history.get_series(['pres']):
+	date = day['datetime'].strftime("%m/%d/%y")
+	print("Pressure on " + date, day['pres'] / 33.864, "inHg") #convert milibar to inHg
+
+history = api.get_history(city="Carmel", state="Indiana", country="US", start_date="2020-10-21", end_date="2020-10-22")
+for day in history.get_series(['pres']):
+	date = day['datetime'].strftime("%m/%d/%y")
+	print("Pressure on " + date, day['pres'] / 33.864, "inHg") #convert milibar to inHg
+
+
+
+forecast = api.get_forecast(city="Carmel", state="Indiana", country="US")
+for day in forecast.get_series(['pres']):
+	date = day['datetime'].strftime("%m/%d/%y")
+	print("Pressure on " + date, day['pres'] / 33.864, "inHg") #convert milibar to inHg
